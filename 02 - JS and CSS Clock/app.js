64ec8f79665a63e826bfed594c0157ac8f84a0b3
Martin Andrade <martin.andrade@sap.com>
2026-01-18 19:21:48 -0300

    WIP taylor swift clock lol

---
+++ 02 - JS and CSS Clock/app.js
@@ -0,0 +1,15 @@
+const runClock = () => {
+    const now = new Date();
+    const secondHand = document.querySelector(".second-hand")
+    const minuteHand = document.querySelector(".min-hand")
+    const hourHand = document.querySelector(".hour-hand")
+
+    const secondDegrees = 360 * (now.getSeconds() / 60) + 90
+    const minuteDegrees = 360 * (now.getMinutes() / 60) + 90
+    const hourDegrees = 360 * (now.getHours() / 60) + 90
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+}
+
+setInterval(runClock, 1000)
