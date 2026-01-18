e69d2057f82d3e29d2b3f51360dede35684480c4
Martin Andrade <martin.andrade@sap.com>
2026-01-18 01:47:30 -0300

    WIP

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,8 +1,21 @@
-window.addEventListener('keydown', (e) => {
+const removeTransition = (key => {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+})
+
+const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
-    audio.currentTime = 0
-    audio.play()
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+};
+
+window.addEventListener('keydown', playSound)
+
+  const keys = Array.from(document.querySelectorAll('.key'))
+  keys.forEach(key => {
+    add.addEventListener('transitionend', removeTransition)
   })
