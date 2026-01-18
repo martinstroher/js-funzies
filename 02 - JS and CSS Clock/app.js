87508ab26b1199d2f66bada6a3fbf8542f5df908
Martin Andrade <martin.andrade@sap.com>
2026-01-18 19:56:04 -0300

    Finished Taylor Swift Midnights clock. its wonderful.

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -1,12 +1,20 @@
 const runClock = () => {
     const now = new Date();
-    const secondHand = document.querySelector(".second-hand")
-    const minuteHand = document.querySelector(".min-hand")
-    const hourHand = document.querySelector(".hour-hand")
+    const secondHand = document.querySelector(".second-hand");
+    const minuteHand = document.querySelector(".min-hand");
+    const hourHand = document.querySelector(".hour-hand");
+    const allHands = document.querySelectorAll(".hand");
+
+    if (now.getSeconds() === 0) {
+        allHands.forEach(hand => {hand.style.transition = 'none'});
+    } else {
+        allHands.forEach(hand => {hand.style.transition = ''});
+    }
 
     const secondDegrees = 360 * (now.getSeconds() / 60) + 90
     const minuteDegrees = 360 * (now.getMinutes() / 60) + 90
     const hourDegrees = 360 * (now.getHours() / 12) + 90
+
     secondHand.style.transform = `rotate(${secondDegrees}deg)`
     minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
     hourHand.style.transform = `rotate(${hourDegrees}deg)`
